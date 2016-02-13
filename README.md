6aa7861fce1f51a2fad1e2f0f861886a0da06e1b
tracilopez <tlopez@sungevity.com>
2016-02-12 16:15:21 -0800

    current user schema works

---
+++ README.md
@@ -0,0 +1,11 @@
+# Schema validator
+
+A [Node-based](http://nodejs.com/) single page JSON schema validator for JSON
+
+## Setup
+* Install [nodejs](http://nodejs.org/) and npm for your platform
+
+## Running Schema validator
+
+* Run 'node app.js'
+
--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 var ZSchema = require("z-schema");
 var validator = new ZSchema();
 var fs = require('fs');
+var path = require('path');
 
 var schema = JSON.parse(fs.readFileSync('user.json', "utf8"));
 var json = JSON.parse(fs.readFileSync('user-test.json', "utf8"));
 
-// ZSchema.setSchemaReader(function (uri) {
-//     var someFilename = path.resolve(__dirname, './' + uri + ".json");
-//     console.log(someFilename);
-//     return JSON.parse(fs.readFileSync(someFilename, "utf8"));
-// });
+ZSchema.setSchemaReader(function (uri) {
+    var someFilename = path.resolve(__dirname, uri + ".json");
+    return JSON.parse(fs.readFileSync(someFilename, "utf8"));
+});
 
-var valid = validator.validate(json, schema);
+validator.validate(json, schema, function(err, report) {
+	if (err) {
+		console.log("error");
+		console.log(err);
+	} else {
+		console.log(report);
+	}
+});
 
-console.log(valid);
-console.log(schema);
